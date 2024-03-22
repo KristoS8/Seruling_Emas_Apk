377874d84062412ba5d2069c41b9ca38306b6f1e
Kristo Samosir <kristoisthebest@gmail.com>
2024-03-22 23:09:45 +0900

    menambah tentang aplikasi,perbaiki ui

--- app/src/main/java/com/example/serulingemas/Activity/infoAplikasiActivity.kt
+++ app/src/main/java/com/example/serulingemas/Activity/infoAplikasiActivity.kt
@@ -1,16 +1,33 @@
 package com.example.serulingemas.Activity
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.serulingemas.R
 import com.example.serulingemas.databinding.ActivityInfoAplikasiBinding
 
 private lateinit var binding: ActivityInfoAplikasiBinding
 
-class infoAplikasiActivity : AppCompatActivity() {
+class infoAplikasiActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityInfoAplikasiBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.imgBackFromInfo.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.imgBackFromInfo -> {
+                val intent = Intent()
+                intent.putExtra("key", 1)
+                setResult(Activity.RESULT_OK, intent)
+                finish()
+            }
+        }
     }
 }
